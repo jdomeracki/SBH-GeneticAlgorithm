a22d0561c5d1b67fe09f1631255d06c8929f0ab8
jdomeracki <j.domeracki96@gmail.com>
2020-06-22 00:50:37 +0200

    Results generation automated

--- GA-implementation.py
+++ GA-implementation.py
@@ -19,6 +19,7 @@ if __name__ == "__main__":
     for instance in instances:
         spectrum = get_spectrum("./Instances/" + instance)
         results_file.write(instance + "\n")
+        print(instance)
         N, l, perfect_score, size_of_population = set_parameters(
             instance, spectrum)
         current_best = None
@@ -27,7 +28,6 @@ if __name__ == "__main__":
         pre_check = check_if_perfect(optimized, N)
         if(pre_check >= 0):
             optimal = optimized[pre_check]
-            print(optimal)
             results_file.write("Length: " + str(len(optimal)) + " Score: " +
                                str(perfect_score) + " Accuracy: 100%")
         else:
@@ -45,3 +45,4 @@ if __name__ == "__main__":
                 current_best[4]) + " Accuracy: " + str((current_best[4]/perfect_score)*100) + "%")
         end = timer()
         results_file.write(" Execution time: " + str(end - start) + " [s]\n")
+        print("Execution time: " + str(end - start) + " [s]\n")
